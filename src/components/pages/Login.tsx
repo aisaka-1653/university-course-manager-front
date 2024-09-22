64d2705351c90d14562dbbee52ba3bd6b509d453
aisaka-1653 <reobata1127@gmail.com>
2024-09-22 20:23:15 +0900

    feat: ログインフォームのレイアウトを実装

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,3 +1,83 @@
+import { loginFormSchema, LoginFormSchema } from "@/schema/loginFormSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
 export const Login = () => {
-  return <div>Login</div>
-}
+  const form = useForm<LoginFormSchema>({
+    mode: "onChange",
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = (user: LoginFormSchema) => {
+    console.log(user);
+    form.reset();
+  };
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">ログイン</CardTitle>
+        <CardDescription>Eメールとパスワードを入力してください</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Eメール</FormLabel>
+                  <FormControl>
+                    <Input placeholder="university@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>パスワード</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={!form.formState.isValid}>
+              ログイン
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,13 @@
 import { Login } from "@/components/pages/Login"
+import { AuthLayout } from "@/components/templates/AuthLayout"
 import { Route, Routes } from "react-router-dom"
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="users/login" element={<Login />} />
+      <Route element={<AuthLayout />}>
+        <Route path="users/login" element={<Login />} />
+      </Route>
     </Routes>
   )
 }
