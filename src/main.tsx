62282a07f28e8a6fcbfc02ef5f303ac4dec5a770
aisaka-1653 <reobata1127@gmail.com>
2024-09-22 21:19:31 +0900

    feat: 認証機能用カスタムフックの実装

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import { Router } from "./router/Router"
-import "./index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
+import "./index.css";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Router />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>
-)
+);
