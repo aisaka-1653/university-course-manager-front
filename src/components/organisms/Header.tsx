6dd21cbc90cf8860493020381d16f35489f73e1d
aisaka-1653 <reobata1127@gmail.com>
2024-09-25 21:56:48 +0900

    feat: HeaderとNavigationBarコンポーネントを実装

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,20 @@
+import { useUser } from "@/hooks/useUser";
+import { UserAvatar } from "../molecules/UserAvatar";
+import { Loader2 } from "lucide-react";
+
+export const Header = () => {
+  const { currentUser, isLoading } = useUser();
+
+  return (
+    <div className="flex justify-between items-center bg-blue-500 text-white h-14 px-10">
+      <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight">
+        大学講義管理システム
+      </h1>
+      {isLoading ? (
+        <Loader2 className="h-4 w-4 animate-spin" />
+      ) : (
+        <UserAvatar name={currentUser.full_name} className="gap-4" />
+      )}
+    </div>
+  );
+};
---
+++ src/components/organisms/NavigationBar.tsx
@@ -0,0 +1,34 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
+
+export function NavigationBar() {
+  return (
+    <NavigationMenu className="justify-start bg-blue-400 text-white px-6">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link to="/home" className={navigationMenuTriggerStyle()}>
+            ホーム
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/courses" className={navigationMenuTriggerStyle()}>
+            授業一覧
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link
+            to="/courses/registration"
+            className={navigationMenuTriggerStyle()}
+          >
+            受講登録
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
