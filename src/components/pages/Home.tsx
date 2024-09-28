cb7c80060845afc2eb4288deceed02aec7fe0d35
aisaka-1653 <reobata1127@gmail.com>
2024-09-28 12:35:58 +0900

    feat: Homeページを実装

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,13 @@
-import { useAuth } from "@/hooks/useAuth";
-import { Button } from "../ui/button";
+import { HomeItemList } from "../organisms/HomeItemList";
+import { SectionTitle } from "../atoms/SectionTitle";
 
 export const Home = () => {
-  const { logout } = useAuth();
-  return <Button onClick={logout}>ログアウト</Button>;
+  return (
+    <div className="p-10">
+      <SectionTitle className="mb-4">ホーム</SectionTitle>
+      <HomeItemList className="mb-10" />
+      <SectionTitle className="mb-4">お知らせ</SectionTitle>
+      <p>お知らせはありません</p>
+    </div>
+  );
 };
